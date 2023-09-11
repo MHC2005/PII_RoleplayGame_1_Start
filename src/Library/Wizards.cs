d40a10ab11a0b272677e993e918a0b2c87064d8e
emicostanian <mariaemiliacostanian@gmail.com>
2023-09-11 15:44:10 -0300

    edito Wizard

--- src/Library/Wizards.cs
+++ src/Library/Wizards.cs
@@ -3,3 +3,18 @@ using System.Collections.Generic;
 
 
 namespace Library;
+public class Wizard : Character
+{
+    public List<Spell> Spells { get; set; }
+    public Staff MagicalStaff { get; set; }
+
+    public Wizard(string name) : base(name)
+    {
+        Spells = new List<Spell>();
+    }
+
+    public void LearnSpell(Spell spell)
+    {
+        Spells.Add(spell);
+    }
+}
