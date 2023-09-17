4d733f6ba217eafc55fe75321b5d969f03baa432
emicostanian <mariaemiliacostanian@gmail.com>
2023-09-17 17:46:55 -0300

    creo que lo hice 2.0

--- src/Library/Character.cs
+++ src/Library/Character.cs
@@ -2,27 +2,17 @@ using System;
 using System.Collections.Generic;
 
 namespace Library;
-public class Character
+public interface Character
 {
     public string Name { get; set; }
     public int  MaxHealth { get; set; }
     public int Health { get; set; }
 
-    public Character(string name, int maxHealth)
-    {
-        Name = name;
-        MaxHealth = maxHealth;
-        Health = maxHealth; // Health se inicializa con el valor de MaxHealth
-    }
+    
 
-    public void Attack(Character target, int attackPower)
-    {
-        target.Health -= attackPower;
-    }
+    public void Attack(Character target, int attackPower);
 
-    public void Defend(int defensePower)
-    {
-        this.Health = Math.Min(this.MaxHealth, this.Health + defensePower);
-    }
+    public void Defend(int defensePower);
+    
 }
 
--- src/Library/Wizard.cs
+++ src/Library/Wizard.cs
@@ -5,15 +5,32 @@ using System.Collections.Generic;
 namespace Library;
 public class Wizard : Character
 {
+    public string Name { get; set; }
+    public int MaxHealth { get; set; }
+    public int Health { get; set; }
     public List<Spell> Spells { get; set; }
     public List<Item> WizardItems { get; set; }
+    
 
-    public Wizard(string name, int maxHealth, int power) : base(name, maxHealth)
+    public Wizard(string name, int maxHealth, int power) 
     {
+        Name = name;
+        MaxHealth = maxHealth;
+        Health = maxHealth;
         Spells = new List<Spell>();
         WizardItems = new List<Item>(); // Inicializa la lista WizardItems
     }
     
+    public void Attack(Character target, int attackPower)
+        {
+            target.Health -= attackPower;
+        }
+
+        public void Defend(int defensePower)
+        {
+            Health = Math.Min(MaxHealth, Health + defensePower);
+        }
+
     public void AddThings(Item item)
     {
         WizardItems.Add(item);
