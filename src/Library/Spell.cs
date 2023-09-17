fddd987bb593766806edeb855e22e671dc1daa18
emicostanian <mariaemiliacostanian@gmail.com>
2023-09-17 16:43:43 -0300

    ya esta

--- src/Library/Spell.cs
+++ src/Library/Spell.cs
@@ -2,15 +2,15 @@ using System;
 using System.Collections.Generic;
 
 namespace Library;
-public class Spell
+public class Spell 
 {
     public string Name { get; set; }
-    public int power {get; set;}
+    public int Power {get; set;}
 
     public Spell(string name, int power)
     {
         this.Name = name;
-        this.Power= power;
+        this.Power = power;
     }
 
     public void Cast(Character target)
