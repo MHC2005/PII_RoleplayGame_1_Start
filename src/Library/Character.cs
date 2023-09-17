b4642e29147e3fe73e12163afec85d135200e518
francofil <francofil03@gmail.com>
2023-09-17 18:59:53 -0300

    Ultimo

--- src/Library/Character.cs
+++ src/Library/Character.cs
@@ -7,7 +7,7 @@ public interface Character
     public string Name { get; set; }
     public int  MaxHealth { get; set; }
     public int Health { get; set; }
-
+    
     
 
     public void Attack(Character target, int attackPower);
