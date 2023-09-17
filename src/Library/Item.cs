8b19afcfb0094263f3c4d04a98a64a7112d1948f
emicostanian <mariaemiliacostanian@gmail.com>
2023-09-17 18:19:45 -0300

    cambios chiquitos

--- src/Library/Item.cs
+++ src/Library/Item.cs
@@ -7,7 +7,7 @@ public interface Item //sería el arma o el libro de hechizos o lo que sea
     public string Name { get; set; }
     public int Power { get; set; }
 
-    public int defensa { get; set; }
+    public int Defensa { get; set; }
 
     
 }
--- src/Library/Staff.cs
+++ src/Library/Staff.cs
@@ -8,10 +8,11 @@ public class Staff : Item
 
     public string Name { get; set; }
     public int Power { get; set; }
-    public int defensa { get; set; }
-    public Staff(string name, int power)  //estas son las cosas, tipo armas y libros
+    public int Defensa { get; set; }
+    public Staff(string name, int power, int defensa)  //estas son las cosas, tipo armas y libros
     {
         this.Name = name;
         this.Power = power;
+        this.Defensa = defensa;
     }
 }
