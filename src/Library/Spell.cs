781bbf46683956c066bf3ec81861c1da54fb1f8b
emicostanian <mariaemiliacostanian@gmail.com>
2023-09-17 16:46:52 -0300

    asi te gusta mas?

--- src/Library/Spell.cs
+++ src/Library/Spell.cs
@@ -2,12 +2,12 @@ using System;
 using System.Collections.Generic;
 
 namespace Library;
-public class Spell 
+public class Spell : Item
 {
-    public string Name { get; set; }
-    public int Power {get; set;}
+    //public string Name { get; set; }
+    //public int Power {get; set;}
 
-    public Spell(string name, int power)
+    public Spell(string name, int power) : base(name, power)
     {
         this.Name = name;
         this.Power = power;
