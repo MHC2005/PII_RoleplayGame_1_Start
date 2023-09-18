f6e8b5395eb6dfacebf22606c58d69dd4238a19d
maruuroman <maria.roman@correo.ucu.edu.uy>
2023-09-18 09:25:21 -0300

    testeo de salud maxima

--- src/Test/Library.Test/ExampleTest.cs
+++ src/Test/Library.Test/ExampleTest.cs
@@ -5,6 +5,13 @@ namespace Test.Library
     public class ExampleTest
     {
         [Test]
+        public void SupiriorMaxHealth()
+        {
+            string input = MaxHealth>100;
+            string expected = MaxHealth==100;
+            string output = MaxHealth==100;
+            Assert.AreEqual(output, expected);
+        }
         public void dummyTest()
         {
             Assert.True(true);
