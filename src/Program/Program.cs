c88c020d326a3ea037021bf705ba46139e239497
francofil <francofil03@gmail.com>
2023-09-18 08:20:59 -0300

    Ultima

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -42,10 +42,10 @@ namespace Program
             frankie.Attack(emi, espada);   // El enano ataca al mago "Emi" con su hacha
 
 
-            Console.WriteLine($"{maru.Name}: Health = {maru.Health} Total Defense = {maru.DefensaTotal}");
-            Console.WriteLine($"{emi.Name}: Health = {emi.Health}");
-            Console.WriteLine($"{mateito.Name}: Health = {mateito.Health}");
-            Console.WriteLine($"{frankie.Name}: Health = {frankie.Health}");
+            Console.WriteLine($"{maru.Name}: Health = {maru.Health} Total Defense = {maru.DefensaTotal()}");
+            Console.WriteLine($"{emi.Name}: Health = {emi.Health} Total Defense = {emi.DefensaTotal()}");
+            Console.WriteLine($"{mateito.Name}: Health = {mateito.Health} Total Defense = {mateito.DefensaTotal()}");
+            Console.WriteLine($"{frankie.Name}: Health = {frankie.Health} Total Defense = {frankie.DefensaTotal()}");
 
             Console.ReadLine();
         }
